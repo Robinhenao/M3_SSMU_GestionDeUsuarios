3df2aa490c27f0b04b10eda0414e38412255d8b1
Robinhenao <65988533+Robinhenao@users.noreply.github.com>
2023-09-25 15:20:50 -0500

    Configuración ambiente para Backend

---
+++ src/test/java/com/udea/ssmu/GestionDeUsuarios/GestionDeUsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udea.ssmu.GestionDeUsuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionDeUsuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
